af2ed3e2db786c6ed805d3fcf39af1a76c095039
Bhavay Singhal <bhavaysinghal01@gmail.com>
2023-01-04 23:48:30 +0530

    added react build and deployment code

--- backend/server.js
+++ backend/server.js
@@ -6,6 +6,7 @@ const chatRoutes = require("./routes/chatRoutes");
 const messageRoutes = require("./routes/messageRoutes");
 const { notFound, errorHandler } = require("./middleware/errorMiddleware");
 const connectDB = require("./config/db");
+const path = require("path");
 const app = express();
 // to accept json data
 app.use(express.json());
@@ -24,7 +25,19 @@ app.use("/api/chat", chatRoutes);
 app.use("/api/message", messageRoutes);
 
 // --------------------------deployment------------------------------
+const __dirname1 = path.resolve();
 
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname1, "../frontend/build")));
+
+  app.get("*", (req, res) =>
+    res.sendFile(path.resolve(__dirname1, "../frontend", "build", "index.html"))
+  );
+} else {
+  app.get("/", (req, res) => {
+    res.send("API is running..");
+  });
+}
 // --------------------------deployment------------------------------
 
 // error handling middlewares in the all the the routes other than the sprecified will be handeled
