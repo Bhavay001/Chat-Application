4847cadff953b6a3ba1b1c78276af4f9c536fdcf
Bhavay Singhal <bhavaysinghal01@gmail.com>
2023-01-04 18:27:13 +0530

    adding comment

--- frontend/src/Pages/Homepage.js
+++ frontend/src/Pages/Homepage.js
@@ -14,6 +14,7 @@ import Login from "../components/Authentication/Login";
 import SignUp from "../components/Authentication/SignUp";
 
 const Homepage = () => {
+  // it is used for navigate
   const history = useHistory();
   useEffect(() => {
     const user = JSON.parse(localStorage.getItem("userInfo"));
