09db2d0fd53a3ac63e54f6d890fd0a9784b8f795
Bhavay Singhal <bhavaysinghal01@gmail.com>
2023-01-09 22:50:29 +0530

    socket url change

--- backend/server.js
+++ backend/server.js
@@ -53,7 +53,7 @@ const server = app.listen(PORT, () => {
 const io = require("socket.io")(server, {
   pingTimeout: 60000,
   cors: {
-    origin: "http://localhost:3000",
+    origin: `${process.env.FRONTEND_URL}`,
   },
 });
 // io.on will listen to everyone make a socket instance for bhavay,yuvraj,tushar
